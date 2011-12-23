f22ad3c2b114dbc8fff49b8cde6cba686f3127a5
pylight <intelligenzfluechtling@gmail.com>
2011-12-23 21:31:54 +0100

    randompost-button works now

---
+++ js/randomPost.js
@@ -0,0 +1,22 @@
+---
+layout: javascript
+---
+function getRandomPost()
+{
+	var posts = new Array();
+
+	/* fill the new array */
+	posts =   
+	[
+	{% for post in site.posts %}
+		"{{ site.url }}{{ post.url }}",
+	{% endfor %}
+	""
+	];
+	
+	/* get one random element */
+	var randValue = Math.floor(Math.random() * posts.length - 1 );
+	
+	/* redirect to the random post */
+	window.location = posts[randValue];
+}
